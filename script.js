1aa61c40fe01e909a95e4a9ca63b1d3e5398c050
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-05-05 23:30:31 +0530

    generate rgb color on button click

--- script.js
+++ script.js
@@ -25,23 +25,24 @@ const getRedInputRange = document.querySelector('#red');
 const getGreenInputRange = document.querySelector('#green');
 const getBlueInputRange = document.querySelector('#blue');
 const rgbColorContainer = document.querySelector('.rgb-color-container');
+const rgbText = document.querySelector('.rgb-text');
 
 rgbBtn.addEventListener('click', () => {
-    debugger
     let extractedRedValue = getRedInputRange.value;
     let extractedGreenValue = getGreenInputRange.value;
     let extractedBlueValue = getBlueInputRange.value;
-
+    rgbText.textContent = `rgb(${extractedRedValue},${extractedGreenValue},${extractedBlueValue})`;
     rgbColorContainer.style.backgroundColor = `rgb(${extractedRedValue},${extractedGreenValue},${extractedBlueValue})`;
 });
 
 hexCopyBtn.addEventListener('click', copyHexColorToClipBoard)
 
+rgbCopyBtn.addEventListener('click', copyRgbColorToClipBoard)
 
 function copyHexColorToClipBoard() {
     navigator.clipboard.writeText(hexColorValue.textContent);
 }
 
 function copyRgbColorToClipBoard() {
-
+    navigator.clipboard.writeText(`rgb(${getRedInputRange.value},${getGreenInputRange.value},${getBlueInputRange.value})`);
 }
